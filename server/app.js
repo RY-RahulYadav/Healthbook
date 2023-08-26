c3c06b8292119e2cdd89f98b4ca8f7ea9b22ff46
RY-RahulYadav <ry.rahul036@gmail.com>
2023-08-26 22:11:26 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = process.env.PORT||3000
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/" , (req,res)=>{
+
+    res.send('<h1 style="text-align:center">Welcome to my healthcare server</h1>');
+})
+
+app.listen( port  , (err)=>{
+  if(err){
+    console.log(err);
+  }
+
+  else{
+    console.log("server start");
+
+  }
+})
+
